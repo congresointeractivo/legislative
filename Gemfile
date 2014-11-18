f078e027f0505a1c2cc6e8c4215abae13ff05a25
@martinszy <martinsz@gmail.com>
2014-11-18 00:41:07 +0000

    Adding pry. Making main search return congressmen.

--- Gemfile
+++ Gemfile
@@ -77,6 +77,7 @@ group :development, :test do
   gem 'guard-bundler'
   gem 'guard-rails'
   gem 'guard-rspec'
+  gem 'pry-rails'
 end
 
 group :test do
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -13,19 +13,6 @@ class SearchesController < ApplicationController
     coder = HTMLEntities.new
     
     if !ENV['popit_persons'].blank? and !ENV['popit_organizations'].blank? and !ENV['billit_url'].blank? and !ENV['popit_search'].blank?
-      response = HTTParty.get(ENV['popit_persons'])
-      json_response = JSON.parse(response.body)
-      authors_detail_list = json_response['result']
-
-      @authors_list = []
-      @persons_query = []
-      authors_detail_list.map do |author|
-        @authors_list.push(author['name'])
-      end
-
-      @organizations = Popit::OrganizationCollection.new
-      @organizations.get ENV['popit_organizations']+'?per_page=100', 'application/json'
-
       @congressmen = PopitPersonCollection.new
 
       if !params.nil? && params.length > 3 # default have 3 keys {'action'=>'index', 'controller'=>'searchs', "locale"=>"xx"}
@@ -47,16 +34,10 @@ class SearchesController < ApplicationController
             @keywords << key + '="' + coder.encode(value) + '"&'
           end
         end
-        if params['authors'] != nil
-          @authors_list = []
-          authors_detail_list.map do |author|
-            if params['authors'] == author['name']
-              @persons_query.push(author)
-            end
-          end
-        end
+
+        p  ENV['popit_search']+"?#{URI.encode(@keywords)}per_page=3"
         @bills_query = Billit::BillPage.get(ENV['billit_url'] + "search.json/?#{URI.encode(@keywords)}per_page=3", 'application/json')
-        @congressmen.get ENV['popit_search']+"#{URI.encode(@keywords)}per_page=3", 'application/json'
+        @congressmen.get ENV['popit_search']+"?#{URI.encode(@keywords)}per_page=3", 'application/json'
       else
         @bills_query = Billit::BillPage.get(ENV['billit_url'] + "search.json/?per_page=3", 'application/json')
         @congressmen.get ENV['popit_search']+"per_page=3", 'application/json'
