3917967b9ea1657a4661c60b20c57e9d36cd4a67
to-more <to_more@mailoo.org>
2014-12-03 20:14:52 -0300

    add use of pad variables

--- app/controllers/glossaries_controller.rb
+++ app/controllers/glossaries_controller.rb
@@ -13,11 +13,7 @@ class GlossariesController < ApplicationController
   caches_page :index
   # GET /glossaries
   def index
-    @padurl = "http://pad.congresointeractivo.org/"
-    @apikey = "2064063a3291c4a32b47e9c636649dea83d918ff6a9a87467393d5bcb2bb7be8"
-
     @pads = get_list
-    
     @pads
   end
   
--- app/helpers/glossaries_helper.rb
+++ app/helpers/glossaries_helper.rb
@@ -1,13 +1,12 @@
 module GlossariesHelper
 
   def get_pad padid = nil
-
     @pad = { id: params[:id] } 
 
-    ether = EtherpadLite.connect @padurl, @apikey, '1.2.1'
+    ether = EtherpadLite.connect ENV['padurl'], ENV['apikey'], '1.2.1'
     pad = ether.pad "metodologia"
     
-    @pad[:edit_url] = "#{@padurl}p/metodologia"
+    @pad[:edit_url] = "#{ENV['padurl']}p/metodologia"
 
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, autolink: true, tables: true)
 
@@ -16,18 +15,17 @@ module GlossariesHelper
   end
 	
   def get_list
-   page = Nokogiri::HTML open "http://pad.congresointeractivo.org/list"
+   page = Nokogiri::HTML open "#{ENV['padurl']}list"
    links = page.css "a"
 
-   ether = EtherpadLite.connect @padurl, @apikey, '1.2.1'
-   binding.pry
+   ether = EtherpadLite.connect ENV['padurl'], ENV['apikey'], '1.2.1'
    pad_text = ""
    pads = []
    links.each do | link |
       pad = ether.pad link.text
       puts "Id:#{link.text} texto:#{pad.text}" unless pad.text
       pad_text = pad.text
-      pads << {id: link.text, text: pad.text, edit_url: "#{@padurl}p/#{link.text}"}
+      pads << {id: link.text, text: pad.text, edit_url: "#{ENV['padurl']}p/#{link.text}"}
    end
    pads
   end
